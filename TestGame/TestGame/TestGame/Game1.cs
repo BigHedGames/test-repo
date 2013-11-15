5c474c8eb1f63fbb7d71fe711ede13c1fea99851
bighed <bighed.games@gmail.com>
2013-11-14 20:33:28 -0800

    What the fuck dude?? I REALLY want the background to be black!!!

--- TestGame/TestGame/TestGame/Game1.cs
+++ TestGame/TestGame/TestGame/Game1.cs
@@ -88,7 +88,7 @@ namespace TestGame
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Cornsilk);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
 
